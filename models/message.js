db0f62dcbfb893d9b9a24e8488e5b9d3c0cbbde8
Andrew Danger Lyon <orthecreedence@gmail.com>
2016-05-24 13:44:03 -0700

    users can now initiate a registration with votebot by texting anything to the number directly

--- models/message.js
+++ models/message.js
@@ -78,56 +78,8 @@ exports.broadcast = function(conversation_id, message)
 
 exports.incoming_sms = function(data)
 {
-	// example data:
-	//
-	// -- txt msg:
-	// incoming{
-	// 	"ToCountry": "US",
-	// 	"ToState": "NJ",
-	// 	"SmsMessageSid": "SMdcbbdeafe24fdf8d5df03c8cff2130be",
-	// 	"NumMedia": "0",
-	// 	"ToCity": "",
-	// 	"FromZip": "55805",
-	// 	"SmsSid": "SMdcbbdeafe24fdf8d5df03c8cff2130be",
-	// 	"FromState": "MN",
-	// 	"SmsStatus": "received",
-	// 	"FromCity": "DULUTH",
-	// 	"Body": "Hi",
-	// 	"FromCountry": "US",
-	// 	"To": "+18565551111",
-	// 	"ToZip": "",
-	// 	"NumSegments": "1",
-	// 	"MessageSid": "SMdcbbdeafe24fdf8d5df03c8cff2130be",
-	// 	"AccountSid": "AC008362520019324312e64264dcf9f8f6",
-	// 	"From": "+12185551234",
-	// 	"ApiVersion": "2010-04-01"
-	// }
-	//
-	// -- media msg:
-	// incoming{
-	// 	"ToCountry": "US",
-	// 	"MediaContentType0": "image/jpeg",
-	// 	"ToState": "NJ",
-	// 	"SmsMessageSid": "MM5129e06796ac063ae721e0fe4cb913c3",
-	// 	"NumMedia": "1",
-	// 	"ToCity": "",
-	// 	"FromZip": "55805",
-	// 	"SmsSid": "MM5129e06796ac063ae721e0fe4cb913c3",
-	// 	"FromState": "MN",
-	// 	"SmsStatus": "received",
-	// 	"FromCity": "DULUTH",
-	// 	"Body": "Zulu",
-	// 	"FromCountry": "US",
-	// 	"To": "+18565551111",
-	// 	"ToZip": "",
-	// 	"NumSegments": "1",
-	// 	"MessageSid": "MM5129e06796ac063ae721e0fe4cb913c3",
-	// 	"AccountSid": "AC008362520019324312e64264dcf9f8f6",
-	// 	"From": "+12185551234",
-	// 	"MediaUrl0": "https://api.twilio.com/2010-04-01/Accounts/AC008362520019324312e64264dcf9f8f6/Messages/MM5129e06796ac063ae721e0fe4cb913c3/Media/ME384328ea2eb342f1e066e260d9d4bf62",
-	// 	"ApiVersion": "2010-04-01"
-	// }
 	var user;
+	log.info('msg: incoming: from: '+data.From+' -- '+data.Body);
 	return user_model.upsert({username: user_model.parsenum(data.From)})
 		.then(function(_user) {
 			user = _user;
@@ -136,14 +88,22 @@ exports.incoming_sms = function(data)
 			return convo_model.get_recent_by_user(user.id);
 		})
 		.then(function(conversation) {
-			if(!conversation) throw error('user is in no conversations');
-			return exports.create(user.id, conversation.id, {body: data.Body})
-				.tap(function(message) {
-					if(conversation.type == 'bot')
-					{
-						return bot_model.next(user.id, conversation, message)
-					}
-				});
+			if(conversation)
+			{
+				log.info('msg: incoming: continuing existing voter reg');
+				return exports.create(user.id, conversation.id, {body: data.Body})
+					.tap(function(message) {
+						if(conversation.type == 'bot')
+						{
+							return bot_model.next(user.id, conversation, message)
+						}
+					});
+			}
+			else
+			{
+				log.info('msg: incoming: starting new voter reg');
+				return bot_model.start('vote_1', user.id, {start: 'intro_direct'});
+			}
 		});
 };
 
