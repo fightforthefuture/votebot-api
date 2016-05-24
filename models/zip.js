0a1a08b713fbeaf87ae9429e5c86014bab170a63
Andrew Danger Lyon <orthecreedence@gmail.com>
2016-05-23 21:37:15 -0700

    adding zip code api. its free and easy. eventually we can do some kind of baked-in database, but we are prototyping here, no need to get crazy.

---
+++ models/zip.js
@@ -0,0 +1,36 @@
+var Promise = require('bluebird');
+var request = require('request');
+
+exports.find = function(zip)
+{
+	return new Promise(function(resolve, reject) {
+		request('http://api.zippopotam.us/us/'+zip, function(err, res, body) {
+			if(err) return reject(err);
+			if(res.statusCode >= 400) return reject(data_error('That zipcode wasn\'t found'));
+			try
+			{
+				var obj = JSON.parse(body) || {};
+
+				// normalize our zip data
+				var zipdata = {
+					code: obj['post code'],
+					places: (obj.places || [])
+						.map(function(place) {
+							if(!place) return false;
+							return {
+								city: place['place name'],
+								state: place['state abbreviation']
+							}
+						})
+						.filter(function(p) { return !!p; })
+				};
+			}
+			catch(e)
+			{
+				return reject(e);
+			}
+			resolve(zipdata);
+		});
+	});
+};
+
