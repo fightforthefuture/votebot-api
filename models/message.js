75f84e91714f75c7c730e2435a29411387a56934
Josh Levinger <josh@levinger.net>
2016-07-22 12:11:25 -0700

    switch message create to use service sid, so we can go multi-channel

--- models/message.js
+++ models/message.js
@@ -71,7 +71,7 @@ exports.broadcast = function(conversation_id, message)
 				log.info('sending sms to '+to);
 				return twilio.messages.createAsync({
 					to: to,
-					from: config.twilio.from_number,
+					messaging_service_sid: config.twilio.messaging_sid,
 					body: message.body
 				});
 			}));
