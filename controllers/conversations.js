db12874b25019afc1a939ab97e1a611dc362c623
Jeff Lyon <jeff@rubbingalcoholic.com>
2016-08-29 13:01:31 -0700

    more blindfire bs

--- controllers/conversations.js
+++ controllers/conversations.js
@@ -24,6 +24,7 @@ var create = function(req, res)
 {
 	var user_id = config.bot.user_id;
 	var data = req.body;
+	console.log('JL DEBUG ~ ', data);
 	model.create(user_id, data)
 		.then(function(convo) {
 			resutil.send(res, convo);
