6d61b0a54ff1fbcebd32ca0ca1f6f12ea8bd7116
Jeff Lyon <jeff@rubbingalcoholic.com>
2016-09-02 14:07:13 -0700

    broke facebook messaging into its own model

---
+++ models/facebook.js
@@ -0,0 +1,36 @@
+var Promise = require('bluebird');
+var rp = require('request-promise');
+
+var sendMessage = function(payload) {
+	payload.uri = 'https://graph.facebook.com/v2.6/me/messages?access_token='+config.facebook.access_token;
+	payload.method = 'POST';
+
+	return rp(payload)
+	    .then(function (body) {
+	        return Promise.resolve(body);
+	    })
+	    .catch(function (err) {
+	        return Promise.reject(err);
+	    });
+}
+
+exports.buttons = function(username, text, buttons) {
+	var message = {
+		json: {
+			recipient: {
+				id: username.replace('Messenger:', '')
+			},
+			message: {
+				attachment: {
+					type: 'template',
+					payload: {
+						template_type: 'button',
+						text: text,
+						buttons: buttons
+					}
+				}
+			}
+		}
+	}
+	return sendMessage(message);
+}
