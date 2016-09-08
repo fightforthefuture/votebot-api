ea085d7919e75c493cf73aa3212b6e5ba9863035
Jeff Lyon <jeff@rubbingalcoholic.com>
2016-09-08 09:01:44 -0700

    replace zippopotamus with smartystreets

--- models/zip.js
+++ models/zip.js
@@ -1,36 +1,42 @@
 var Promise = require('bluebird');
 var request = require('request');
+var config = require('../config');
 
 exports.find = function(zip)
 {
 	return new Promise(function(resolve, reject) {
-		request('http://api.zippopotam.us/us/'+zip, function(err, res, body) {
-			if(err) return reject(err);
-			if(res.statusCode >= 400) return reject(new Error('not_found'));
-			try
-			{
-				var obj = JSON.parse(body) || {};
+        var req_url = 'https://us-zipcode.api.smartystreets.com/lookup'
+            +'?auth-id='+config.smarty_streets.auth_id
+            +'&auth-token='+config.smarty_streets.auth_token
+            +'&zipcode='+zip;
+        request(req_url, function(err, res, body) {
+            if(err) return reject(err);
+            if(res.statusCode >= 400) return reject(new Error('not_found'));
+            try
+            {
+                var obj = JSON.parse(body) || {};
+                obj = obj[0];
 
 				// normalize our zip data
 				var zipdata = {
-					code: obj['post code'],
-					places: (obj.places || [])
-						.map(function(place) {
-							if(!place) return false;
+					code: zip,
+					places: (obj.city_states || [])
+						.map(function(city_state) {
+							if(!city_state) return false;
 							return {
-								city: place['place name'],
-								state: place['state abbreviation']
+								city: city_state['city'],
+								state: city_state['state_abbreviation']
 							}
 						})
 						.filter(function(p) { return !!p; })
 				};
-			}
-			catch(e)
-			{
-				return reject(e);
-			}
-			resolve(zipdata);
-		});
-	});
+            }
+            catch(e)
+            {
+                return reject(e);
+            }
+            resolve(zipdata);
+        });
+    });
 };
 
