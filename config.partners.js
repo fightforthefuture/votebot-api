a1764a27967e90d40c39c570c3c38ca7f69ffc4a
Jeff Lyon <jeff@rubbingalcoholic.com>
2016-09-19 12:48:43 -0700

    added more partners

--- config.partners.js
+++ config.partners.js
@@ -1,7 +1,28 @@
 module.exports = {
+    care2: {
+        intro_shortcode: 'care2',
+    },
+    dailykos: {
+        intro_shortcode: 'dailykos',
+    },
+    genius: {
+        intro_shortcode: 'genius',
+    },
     headcount: {
         intro_shortcode: 'count',
         msg_intro: "Hi this is HelloVote and HeadCount! I'm going to help you register to vote. I'll ask a few questions to fill out your registration form.",
         msg_intro_es: "Hola, this could be localized."
-    }
+    },
+    krcla: {
+        intro_shortcode: 'krcla',
+    },
+    nextgen: {
+        intro_shortcode: 'nextgen',
+    },
+    presente: {
+        intro_shortcode: 'presente',
+    },
+    seiuca: {
+        intro_shortcode: 'seiuca',
+    },
 };
