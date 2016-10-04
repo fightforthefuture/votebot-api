97ecbdaf2d30e5237503defbe7c179a73669e3eb
Josh Levinger <josh@levinger.net>
2016-10-03 18:09:37 -0700

    promisify twilio-bindings

--- tools/twilio-bindings-fix.js
+++ tools/twilio-bindings-fix.js
@@ -27,13 +27,10 @@ var fix_notify_bindings = function(stack) {
     }
 
     var user = stack.shift();
-    if ( !user.first_name || !user.last_name ) {
-        console.log('Skipping user ', user.id, ' no name...');
+    if (!user.settings.notify_binding_sid) {
+        console.log('Skipping user ', user.id, ' no notify binding...');
         return fix_notify_bindings(stack);
     }
-
-//    console.log('Fixing notify bindings for user: ', user.id);
-
     return user_model.get(user.id).then(function(user) {
         if (!user_model.use_notify(user.username)) {
             console.log('- unable to fix bindings for ', user.username);
@@ -42,32 +39,32 @@ var fix_notify_bindings = function(stack) {
 
         if(user.active = false) {
             //actually remove binding
-            //console.log('- delete binding for', user.id);
-            try {
-                notify.delete_binding(user);
-            } catch(err) {
+            notify.delete_binding(user).then(function(sid) {
+                console.log('- deleted binding', sid);
+            }, function(error) {
+                console.log(error);
                 console.error('- unable to delete binding', user.id);
-            }
+            });
         } else {
             // update completion tag with specific method
             // use same if-then structure as bot
 
             var form_type = util.object.get(user, 'settings.submit_form_type');
             var mail_eta = util.object.get(user, 'settings.nvra_mail_eta');
-            try {
-                if (form_type != 'NVRA') {
-                    notify.add_tags(user, ['votebot-completed-ovr']);
+            if (form_type != 'NVRA') {
+                tags.push('votebot-completed-ovr');
+            } else {
+                if (mail_eta) {
+                    tags.push('votebot-completed-mail');
                 } else {
-                    if (mail_eta) {
-                        notify.add_tags(user, ['votebot-completed-mail']);
-                    } else {
-                        notify.add_tags(user, ['votebot-completed-pdf']);
-                    }
+                    tags.push('votebot-completed-pdf');
                 }
-                //console.log(' - fixed binding for ', user.id);
-            } catch(err) {
-                console.err('- unable to update binding for ', user.id);
             }
+            notify.add_tags(user, tags).then(function(sid) {
+                console.log('new sid', sid);
+            }, function(error) {
+                console.err('- unable to update binding for ', user.id);
+            });
         }
 
         // mark fixed, so we don't double dip
