179ace38d0bf39cce71e8b3f6932364ae793ff1b
Josh Levinger <josh@levinger.net>
2016-10-07 09:33:16 -0700

    remove fancy apostrophe in partners

--- config.partners.js
+++ config.partners.js
@@ -1,55 +1,55 @@
 module.exports = {
     '18mr': {
         intro_shortcode: '18mr',
-        msg_intro: "(Hi, this is 18MR and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is 18MR and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     aarw: {
         intro_shortcode: 'aarw',
-        msg_intro: "(Hi, this is AARW and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is AARW and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     bazta: {
         intro_shortcode: 'bazta',
-        msg_intro: "(Hi, this is Bazta Arpaio and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Bazta Arpaio and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     bernie: {
         intro_shortcode: 'bernie',
-        msg_intro: "(Hi, this is Senator Sanders and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Senator Sanders and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     care2: {
         intro_shortcode: 'care2',
-        msg_intro: "(Hi, this is Care2 and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Care2 and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     commoncause: {
         intro_shortcode: 'common',
-        msg_intro: "(Hi, this is Common Cause and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Common Cause and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     credo: {
         intro_shortcode: 'credo',
-        msg_intro: "(Hi, this is CREDO Action and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is CREDO Action and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     dailykos: {
         intro_shortcode: 'dailykos',
-        msg_intro: "(Hi, this is DailyKos and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is DailyKos and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     ecovote: {
         intro_shortcode: 'ecovote',
-        msg_intro: "(Hi, this is CA LCV and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is CA LCV and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     fairnessproject: {
         intro_shortcode: 'fair',
-        msg_intro: "(Hi, this is The Fairness Project and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is The Fairness Project and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     generalassembly: {
         intro_shortcode: 'ga',
-        msg_intro: "(Hi, this is General Assembly and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",  
+        msg_intro: "(Hi, this is General Assembly and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",  
     },
     genius: {
         intro_shortcode: 'genius',
-        msg_intro: "(Hi, this is Genius and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Genius and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     headcount: {
         intro_shortcode: 'count',
-        msg_intro: "(Hi, this is Headcount and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Headcount and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
         msg_intro_es: "(Saludos, esto es HelloVote y HeadCount. Puedo revisar si está registrado o ayudarle a registrarse para votar. Sus respuestas seran privadas y seguras. Más información: hellovote.org)"
     },
     hellovotetesting: {
@@ -59,178 +59,178 @@ module.exports = {
     },
     hispanic_chamber: {
         intro_shortcode: 'guac',
-        msg_intro: "(Hi, this is the Hispanic Chamber of Commerce and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is the Hispanic Chamber of Commerce and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     iapala: {
         intro_shortcode: 'iapala',
-        msg_intro: "(Hi, this is IAPALA and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is IAPALA and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     jokesforvotes: {
         intro_shortcode: 'jokes',
-        msg_intro: "(Hi, this is Jokes for Votes and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Jokes for Votes and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     jrs: {
         intro_shortcode: 'jrs',
-        msg_intro: "(Hi, this is LVP and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is LVP and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     krcla: {
         intro_shortcode: 'krcla',
-        msg_intro: "(Hi, this is KRC LA and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is KRC LA and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     lvp: {
         intro_shortcode: 'lvp',
-        msg_intro: "(Hi, this is LVP and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is LVP and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     lenny: {
         intro_shortcode: 'lenny',
-        msg_intro: "(Hi, this is Lenny and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Lenny and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     mtv: {
         intro_shortcode: 'mtv',
-        msg_intro: "(Hi, this is MTV and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is MTV and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     votemtv: {
         intro_shortcode: 'votemtv',
-        msg_intro: "(Hi, this is MTV and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is MTV and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     millionhoodies: {
         intro_shortcode: 'millionhoodies',
-        msg_intro: "(Hi, this is Million Hoodies and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Million Hoodies and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     nextgen: {
         intro_shortcode: 'nextgen',
-        msg_intro: "(Hi, this is NextGen and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is NextGen and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     nextgenca: {
         intro_shortcode: 'cali',
-        msg_intro: "(Hi, this is NextGen CA and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is NextGen CA and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     nextgenlcf: {
         intro_shortcode: 'latinos',
-        msg_intro: "(Hi, this is Latino Community Foundation, NextGen, and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Latino Community Foundation, NextGen, and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     okgo: {
         intro_shortcode: 'okgo',
-        msg_intro: "(Hi, this is OK Go and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is OK Go and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     onearizona: {
         intro_shortcode: 'oneaz',
-        msg_intro: "(Hi, this is One Arizona and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is One Arizona and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     pia: {
         intro_shortcode: 'pia',
-        msg_intro: "(Hi, this is PrivateInternetAccess and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is PrivateInternetAccess and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     pearljam: {
         intro_shortcode: 'pjam',
-        msg_intro: "(Hi, this is Pearl Jam and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Pearl Jam and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     presente: {
         intro_shortcode: 'presente',
-        msg_intro: "(Hi, this is Presente and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Presente and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
         revere_mobile_flow: '57f53979e4b0962618cfecd0'
     },
     refinery29: {
         intro_shortcode: 'r29',
-        msg_intro: "(Hi, this is Refinery29 and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Refinery29 and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     rtv: {
         intro_shortcode: 'rtv',
-        msg_intro: "(Hi, this is Rock the Vote and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Rock the Vote and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     salesforce: {
         intro_shortcode: 'votesalesforce',
-        msg_intro: "(Hi, this is Salesforce and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Salesforce and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     seiuca: {
         intro_shortcode: 'seiuca',
-        msg_intro: "(Hi, this is SEIU CA and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is SEIU CA and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     shaderoom: {
         intro_shortcode: 'theshaderoom',
-        msg_intro: "(Hi, this is The Shade Room and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is The Shade Room and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     surj: {
         intro_shortcode: 'surj',
-        msg_intro: "(Hi, this is SURJ and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is SURJ and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     tab: {
         intro_shortcode: 'tab',
-        msg_intro: "(Hi, this is The Tab and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is The Tab and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     tab: {
         intro_shortcode: 'teamsters',
-        msg_intro: "(Hi, this is Teamsters and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Teamsters and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
         revere_mobile_flow: "57f53c2de4b0962618d0322c"
     },
     transportoakland: {
         intro_shortcode: 'oakland',
-        msg_intro: "(Hi, this is HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     twilio: {
         intro_shortcode: 'twilio',
-        msg_intro: "(Hi, this is Twilio and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Twilio and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     wiuvotes: {
         intro_shortcode: 'wiuvotes',
-        msg_intro: "(Hi, this is the WIU UGC and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",  
+        msg_intro: "(Hi, this is the WIU UGC and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",  
     },
     'wordpress.com': {
         intro_shortcode: 'wordpress',
-        msg_intro: "(Hi, this is WordPress.com and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",  
+        msg_intro: "(Hi, this is WordPress.com and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",  
     },
     
     // celeb partners
     aasif_mandvi: {
         intro_shortcode: 'aasif',
-        msg_intro: "(Hi, this is Aasif Mandvi and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",        
+        msg_intro: "(Hi, this is Aasif Mandvi and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",        
     },
     bj_novak: {
         intro_shortcode: 'bjnovak',
-        msg_intro: "(Hi, this is B.J. Novak and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",        
+        msg_intro: "(Hi, this is B.J. Novak and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",        
     },
     chrissy_teigen: {
         intro_shortcode: 'chrissyteigen',
-        msg_intro: "(Hi, this is Chrissy Teigen and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",        
+        msg_intro: "(Hi, this is Chrissy Teigen and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",        
     },
     daymond_john: {
         intro_shortcode: 'daymondjohn',
-        msg_intro: "(Hi, this is Daymond John and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",          
+        msg_intro: "(Hi, this is Daymond John and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",          
     },
     edward_norton: {
         intro_shortcode: 'norton',
-        msg_intro: "(Hi, this is Edward Norton and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",          
+        msg_intro: "(Hi, this is Edward Norton and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",          
     },
     hunter: {
         intro_shortcode: 'hunter',
-        msg_intro: "(Hi, this is Hunter Walk and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Hunter Walk and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     john_legend: {
         intro_shortcode: 'legend',
-        msg_intro: "(Hi, this is John Legend and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",          
+        msg_intro: "(Hi, this is John Legend and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",          
     },
     kristen_bell: {
         intro_shortcode: 'kristenbell',
-        msg_intro: "(Hi, this is Kristen Bell and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",          
+        msg_intro: "(Hi, this is Kristen Bell and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",          
     },
     mindy: {
         intro_shortcode: 'mindy',
-        msg_intro: "(Hi, this is Mindy Kaling and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Mindy Kaling and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     scooter_braun: {
         intro_shortcode: 'scooterbraun',
-        msg_intro: "(Hi, this is Scooter Braun and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",          
+        msg_intro: "(Hi, this is Scooter Braun and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",          
     },
     sacca: {
         intro_shortcode: 'sacca',
-        msg_intro: "(Hi, this is Chris Sacca and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",
+        msg_intro: "(Hi, this is Chris Sacca and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",
     },
     sophia_bush: {
         intro_shortcode: 'sophiabush',
-        msg_intro: "(Hi, this is Sophia Bush and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",          
+        msg_intro: "(Hi, this is Sophia Bush and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",          
     },
     tristan_walker: {
         intro_shortcode: 'tristanwalker',
-        msg_intro: "(Hi, this is Tristan Walker and HelloVote! I can check to see if you’re registered, and then help you register to vote!)",            
+        msg_intro: "(Hi, this is Tristan Walker and HelloVote! I can check to see if you\'re registered, and then help you register to vote!)",            
     },
 };
