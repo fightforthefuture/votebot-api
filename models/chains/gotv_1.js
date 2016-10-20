ed024201f73618c03858cfabe6d10fa0e4ffb741
Josh Levinger <josh@levinger.net>
2016-10-20 15:33:54 -0700

    weather WIP

--- models/chains/gotv_1.js
+++ models/chains/gotv_1.js
@@ -3,18 +3,32 @@ var language = require('../../lib/language');
 var polling_place_model = require('../polling_place');
 var weather_report_model = require('../weather_report');
 var user_model = require('../user');
-
+var util = require('../../lib/util');
 var moment = require('moment');
 var parse_messy_time = require('parse-messy-time');
-var google_url = require('google-url');
+// var google_url = require('google-url');
 var emojiweather = require('emojiweather');
 
 
 module.exports = {
     intro: {
-        process: function() {
+        process: function(body, user, step, conversation) {
             log.info('bot: gotv 1: intro');
-            return Promise.resolve({'next': 'schedule_polling_place'})
+            if (!util.object.get(user, 'settings.address')) {
+                
+                // temp, fill in with testing data
+                var update_user = util.object.set(user, 'settings', {
+                    address: '1461 Alice St',
+                    city: 'Oakland',
+                    state: 'CA',
+                });
+                user.first_name = "Josh";
+                user_model.update(user.id, update_user);
+
+                //return Promise.resolve({'switch_chain': 'vote_1'})
+            };
+
+            return Promise.resolve({'next': 'schedule_polling_place'});
         }
     },
     schedule_polling_place: {
@@ -22,8 +36,15 @@ module.exports = {
             log.info('bot: gotv: looking up polling place info');
 
             var polling_place = polling_place_model.lookup(user.settings.address, user.settings.city, user.settings.state);
+            log.info('bot: gotv: polling place', polling_place)
+
             var gttp_link = "https://gttp.votinginfoproject.org/#"+
                 encodeURIComponent(user.settings.address+' '+user.settings.city+' '+user.settings.state);
+            // TODO, shorten with goo.gl
+            polling_place.link = gttp_link;
+
+            var update_user = util.object.set(user, 'results.polling_place', polling_place);
+            user_model.update(user.id, update_user);
 
             var msg = "Hey {{first_name}}, it's HelloVote! Election day is tomorrow! "+
             "Your polling place is at {{results.polling_place.address.locationName}} in {{results.polling_place.address.city}}.\n{{results.polling_place.link}}\n"+
@@ -47,14 +68,11 @@ module.exports = {
 
             // weather lookup
             // TODO, calculate days_out from vote_time minus current_time
-            var weather_report = weather_report_model.forecast_day(user.settings.city, user.settings.state);
-            weather_report.icon = weather_report.icon.replace('chance', '') // no room for uncertainty
-                                                     .replace('tstorms', 'tstorm'); // normalize for emojiweather
+            var weather_forecast = weather_report_model.forecast(user.settings.city, user.settings.state);
+            var weather = { adjective: weather_forecast.adjective };
+            weather.emoji = emojiweather(weather.simple_text);
 
-            var weather = { conditions: weather_report.Condition.text.toLowerCase() };
-            weather.emoji = emojiweather(weather.conditions);
-
-            switch(weather_report.icon) {
+            switch(weather.simple_text) {
                 case 'sunny':
                 case 'clear':
                 case 'mostlysunny':
@@ -83,7 +101,7 @@ module.exports = {
             }
 
             var msg = "Okay great. I'll send you a reminder at {{vote_time}} with directions. "+
-            "It might be {{weather.conditions}} {{weather.emoji}} so {{weather.action}}! {{weather.action_emoji}} "+
+            "It might be {{weather.advjective}} {{weather.emoji}} so {{weather.action}}! {{weather.action_emoji}} "+
             "Click here to tell friends you'll be voting! {{share_link}}";
 
             var data = {
--- models/weather_report.js
+++ models/weather_report.js
@@ -2,7 +2,7 @@ var Promise = require('bluebird');
 var request = require('request');
 var config = require('../config');
 
-exports.forecast_day = function(city, state, days_out)
+exports.forecast_adjective = function(city, state, days_out)
 {
     return new Promise(function(resolve, reject) {
          var req_url = 'http://api.apixu.com'
@@ -25,7 +25,46 @@ exports.forecast_day = function(city, state, days_out)
                 var obj = JSON.parse(body) || {};
                 var forecast_day = obj.forecast.forecastday[days_out - 1];
                 if (forecast_day && forecast_day.day) {
-                    return resolve(forecast_day.day);
+                    var condition = forecast_day.day.condition.text;
+                    // simplify apixu conditions, remove uncertainty
+                    simple_text.replace('patchy', '')
+                        .replace('outbreaks', '')
+                        .replace('light', '')
+                        .replace('moderate', '')
+                        .replace('heavy', '')
+                        .replace('showers', '')
+                        .replace('shower', '')
+                        .replace('torrential', '')
+                        .replace('in nearby', '')
+                        .replace('nearby', '')
+                        .replace('in area', '')
+                        .replace('at times', '')
+                        .replace('or', '')
+                        // .replace('with', '')
+                        .trim();
+
+                    // convert to adjective
+                    var adj;
+                    switch(simple_text) {
+                        case 'rain':
+                            adj = 'rainy'; break;
+                        case 'mist':
+                            adj = 'misty'; break;
+                        case 'fog':
+                            adj = 'foggy'; break;
+                        case 'snow':
+                            adj = 'snowy'; break;
+                        case 'sleet':
+                        case 'ice pellets':
+                        case 'freezing rain':
+                            adj = 'sleeting'; break;
+                        default:
+                            adj = simple_text; break;
+                    }
+
+                    return resolve({condition: condition,
+                                    simple_text: simple_text,
+                                    adjective: adj});
                 }
             }
             catch(e)
